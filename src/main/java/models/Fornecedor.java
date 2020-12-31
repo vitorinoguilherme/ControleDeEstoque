c45fb31c4bb4d34c4d58558065808aaa65f8fd9b
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2020-12-31 00:32:18 -0300

    Implementado Classe Fornecedor

---
+++ src/main/java/models/Fornecedor.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Fornecedor {
+    //private static final AtomicInteger count = new AtomicInteger(0);
+    private Integer codigo;
+    private String razaoSocial;
+    private String cnpj;
+    private Email email;
+    private Telefone telefone;
+    private CEP cep;
+
+    public Fornecedor(Integer codigo, String razaoSocial, String cnpj, String email, String telefone, String cep) {
+        this.setCodigo(codigo);
+        this.setRazaoSocial(razaoSocial);
+        this.setCnpj(cnpj);
+        this.email = new Email(email);
+        this.telefone = new Telefone(telefone);
+        this.cep = new CEP(cep);
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return "\n CODIGO: "+this.getCodigo()+"" +
+                "\n RAZAO SOCIAL: "+this.getRazaoSocial()+"" +
+                "\n CNPJ: "+this.getCnpj()+"" +
+                "\n E-MAIL: "+this.email.getEmail()+"" +
+                "\n TELEFONE: "+this.telefone.getNumero()+"" +
+                "\n CEP: "+this.cep.getCep()+"";
+    }
+
+
+
+}
