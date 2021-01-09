ad47038dd5f9f35c63eacfd380ea72e3afa49969
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2021-01-09 15:00:00 -0300

    Implementado classe Encomenda

---
+++ src/main/java/models/Encomenda.java
@@ -0,0 +1,92 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Encomenda class model.
+ * This class represents an order for a new product for the store.
+ */
+public class Encomenda {
+    private Integer codigo;
+    private Double frete;
+    private Date dataPedido;
+    private Date dataEntrega;
+    private Cliente cliente;
+    private Transportadora transportadora;
+    private List<ItemEntrada> items;
+
+    /**
+     * This constructor is used by class EncomendaGateway.
+     * @param codigo
+     * @param frete
+     * @param dataPedido
+     * @param dataEntrega
+     * @param codigo_cliente
+     * @param codTransportadora
+     * @param cnpjTransportadora
+     */
+    public Encomenda(Integer codigo, Double frete, Date dataPedido, Date dataEntrega, Integer codigo_cliente,
+                     Integer codTransportadora, String cnpjTransportadora) {
+        this.setCodigo(codigo);
+        this.setFrete(frete);
+        this.setDataPedido(dataPedido);
+        this.setDataEntrega(dataEntrega);
+        this.cliente = cliente;
+        this.transportadora = transportadora;
+        this.items = new ArrayList<>();
+    }
+
+    public Encomenda() {
+
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setFrete(Double frete) {
+        this.frete = frete;
+    }
+
+    public Double getFrete() {
+        return frete;
+    }
+
+    public void setDataPedido(Date dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public Date getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataEntrega(Date dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public Date getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setTransportadora(Transportadora transportadora) {
+        this.transportadora = transportadora;
+    }
+
+    public Transportadora getTransportadora() {
+        return transportadora;
+    }
+}
