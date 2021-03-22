a21de6954c9746b791fbfde30b9919f06bc0a2d1
Guilherme Vitorino <guilherme.vitorino@indraenergia.com>
2021-03-21 21:53:40 -0300

    Removido configuraçÃões de acesso

--- src/main/java/data/ConnectionFactory.java
+++ src/main/java/data/ConnectionFactory.java
@@ -7,9 +7,9 @@ import java.util.Properties;
 public class ConnectionFactory {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/db_estoque?useTimezone=true&serverTimezone=UTC";
-    private static final String USER = "admin";
-    private static final String PASSWORD = "a5b79c5y7?";
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
